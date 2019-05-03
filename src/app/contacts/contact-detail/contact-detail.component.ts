2d0f9c80600a5e1a8b56fbedff9b45a2991503de
remote-upload <remoteupload@git.edu>
2019-05-03 13:30:37 -0600

    done!!

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -7,7 +7,10 @@ import { from } from 'rxjs';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
-  contact: Contact;
+  contact: Contact = new Contact(
+    1, 'Bro. Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null
+  );
+
   constructor() { }
 
   ngOnInit() {
