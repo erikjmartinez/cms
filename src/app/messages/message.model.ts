e096550d9fc70a419f44e88d77713cde6af232e7
remote-upload <remoteupload@git.edu>
2019-05-09 22:35:07 -0600

    YESSSSSSSSSSSSSSSSSSSSSSS!

---
+++ src/app/messages/message.model.ts
@@ -0,0 +1,11 @@
+export class Messages {
+
+    constructor(
+        public messageId: number,
+        public subject: string,
+        public msgText: string,
+        public sender: string) {
+
+    }
+
+}
