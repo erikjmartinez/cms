b620579fb1f77e747246d9f7f4d36409621ada68
remote-upload <remoteupload@git.edu>
2019-05-15 22:35:13 -0600

    ..

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
-
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document(1, 'Exam', 'Exam answers', 'www.google.com', 'No relation'),
+    new Document(2, 'Test', 'Test answers', 'www.google.com', 'No relation'),
+    new Document(3, 'Quiz', 'Quiz answers', 'www.google.com', 'No relation')
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
